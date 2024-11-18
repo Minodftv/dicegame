1eb13b7963b93efc99c909e1ae3bcb4c2986dd03
Minodftv <156418399+Minodftv@users.noreply.github.com>
2024-11-18 16:00:36 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  telegramToken: 'YOUR_TELEGRAM_BOT_TOKEN',
+  infuraUrl: 'YOUR_INFURA_URL',
+  tokenContractAddress: 'YOUR_TOKEN_CONTRACT_ADDRESS',
+  tokenContractABI: 'YOUR_TOKEN_CONTRACT_ABI',
+  botWalletAddress: 'YOUR_BOT_WALLET_ADDRESS',
+};
+
+module.exports = config;
